6e33e355ed4765b5a679fad53b3975aa8433847b
Bhabishya-123 <80686616+Bhabishya-123@users.noreply.github.com>
2021-12-29 15:53:29 +0545

    Add files via upload

---
+++ brain.js
@@ -0,0 +1,36 @@
+//brain used for slideshow image that will change image on click of next and prev button
+var index = 1;
+slideshow(index);
+
+function slideshow(n) {
+    var i;
+    var slide = document.getElementsByClassName("mySlides");
+
+    for (var i = 0; i < slide.length; i++) {
+        slide[i].style.display = 'none';
+    }
+
+    if (n > slide.length) { index = 1 }
+    if (n < 1) { index = slide.length }
+    slide[index - 1].style.display = 'block';
+}
+
+
+function plusSlides(n) {
+    slideshow(index += n);
+}
+
+
+//brain used for auto next image
+var x = 0;
+var images = new Array("./images/0.jpg","./images/1.jpg","./images/2.jpg","./images/3.jpg"); 
+
+function auto()
+  {
+    x++;
+    if (x == images.length)
+       x=0;
+    document.querySelector('.mySlides img').src=images[x];      
+  }
+
+  setInterval(auto, 3000);
